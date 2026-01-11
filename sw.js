309c9fe7e43de64d406c987314179b4605a63b1b
rubnserrano <contactorubenserrano@gmail.com>
2026-01-11 23:14:21 +0100

    Add PWA support: manifest, service worker, offline page and icons

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'hoyno-v1';
+const OFFLINE_URL = 'offline.html';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  OFFLINE_URL,
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
